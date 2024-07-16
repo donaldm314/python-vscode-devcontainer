224a22451b16e9a7571b1033d18417a0d7bd5e06
Don Murray <dmurray@dmurray.ca>
2024-07-16 15:26:42 +0000

    task 3.38

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 deb='task_linux_amd64.deb'
-wget https://github.com/go-task/task/releases/download/v3.37.2/${deb}
+wget https://github.com/go-task/task/releases/download/v3.38.0/${deb}
 sudo dpkg -i ${deb}
 rm -f ${deb}
 
